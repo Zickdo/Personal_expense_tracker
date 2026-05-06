71c8ca95b56e7f4b80c919214c0159651b2e08e2
Isaac-David Ochomma <isaacochomma@gmail.com>
2026-05-06 11:10:26 +0200

    created post actions

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const sqlite3 = require("sqlite3");
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 app.use(express.json());
 
+const expensesDB = new sqlite3.Database("./expenses.db");
+
+app.get("/", (req, res) => {
+  res.send("Hello from my expense app! You guys are amazing!!! ❤️");
+});
+
+app.post("/expenses", (req, res) => {
+  const amount = req.body.amount;
+  const currency = req.body.currency;
+  const description = req.body.description;
+  const date = new Date().toISOString();
+})
 app.listen(PORT, () => {
     console.log( `Server is running on port ${PORT}`);
 });
